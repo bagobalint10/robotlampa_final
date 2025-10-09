65cf555d2a101506f35c3bd895acab6595db5a18
Bagó Bálint <bago.balint10@gmail.com>
2025-10-09 16:30:40 +0200

    port config gombokhoz, meg csak a define + info txt

--- robotlampa_final/main.c
+++ robotlampa_final/main.c
@@ -12,12 +12,39 @@
 #include "segment_write_digit.h"
 #define F_CPU 16000000UL
 #include <util/delay.h>
+
+// port config a gombokhoz 
+
+	#define BTN_UP PORTC		// --> C0  
+	#define BTN_UP PORTC0
+	#define BTN_UP DDRC
+	#define BTN_UP PINC		// bemenet olvasáshoz 
+
+	#define BTN_DOWN PORTC		// --> C1
+	#define BTN_DOWN PORTC1
+	#define BTN_DOWN DDRC
+	#define BTN_DOWN PINC		
+
+	#define BTN_ENTER PORTC		// --> C2
+	#define BTN_ENTER PORTC2
+	#define BTN_ENTER DDRC
+	#define BTN_ENTER PINC		
+
+	#define BTN_MODE PORTC		// --> C30
+	#define BTN_MODE PORTC3
+	#define BTN_MODE DDRC
+	#define BTN_MODE PINC		
+
+
+
+//
 /*
 		gomb bekotes:	 (egy port ra a pinchange miatt majd)
-						mode	- 	 A0-A3 --> PC0 - PC3
-						enter	- 
-						up		- 
-						down	- 
+
+						Up 		- 	A0 - PC0 - 0x01 	
+						Down 	- 	A1 - PC1 - 0x02
+						Enter	-	A2 - PC2 - 0x04 
+						Mode 	- 	A3 - PC3 - 0x08
 */
 //
 
@@ -43,7 +70,7 @@ int main(void)
 		uint8_t buttons = 0;
 		buttons = PORT_Read(&PINC);
 
-		//segment_write_digit(0x01,(uint8_t)(0x01 && 0x00), 0, 1);
+		//segment_write_digit(0x01,'a', 0, 1);
 		segment_write_digit(0x01,(uint8_t) (((buttons&0x01) && 0x01)+48), 0, 1);
 		_delay_ms(1);
 		segment_write_digit(0x02,(uint8_t) (((buttons&0x02) && 0x01)+48), 0, 1);
