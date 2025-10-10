c74740cb6761d66e24e965776e40509d9b9dcdcf
Bagó Bálint <bago.balint10@gmail.com>
2025-10-10 22:44:38 +0200

    button modul létrehozása

---
+++ robotlampa_final/Inc/buttons.h
@@ -0,0 +1,16 @@
+/*
+ * buttons.h
+ *
+ * Created: 2025. 10. 10. 22:43:34
+ *  Author: bagob
+ */ 
+
+
+#ifndef BUTTONS_H_
+#define BUTTONS_H_
+
+
+
+
+
+#endif /* BUTTONS_H_ */
---
+++ robotlampa_final/Src/buttons.c
@@ -0,0 +1,8 @@
+/*
+ * buttons.c
+ *
+ * Created: 2025. 10. 10. 22:43:48
+ *  Author: bagob
+ */ 
+
+ #include "buttons.h"
