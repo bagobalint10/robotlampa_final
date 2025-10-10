b868063852d9077b89087b35f256f5ea97eafb82
Bagó Bálint <bago.balint10@gmail.com>
2025-10-10 16:55:13 +0200

    control_board modulok létrehozása

---
+++ robotlampa_final/Inc/control_board.h
@@ -0,0 +1,16 @@
+/*
+ * control_board.h
+ *
+ * Created: 2025. 10. 10. 16:52:20
+ *  Author: bagob
+ */ 
+
+
+#ifndef CONTROL_BOARD_H_
+#define CONTROL_BOARD_H_
+
+
+
+
+
+#endif /* CONTROL_BOARD_H_ */
---
+++ robotlampa_final/Src/control_board.c
@@ -0,0 +1,20 @@
+/*
+ * control_board.c
+ *
+ * Created: 2025. 10. 10. 16:52:46
+ *  Author: bagob
+ */ 
+
+ #include "control_board.h"
+
+
+
+ void control_board_tmp(void) // ideiglenes sketchi verzio
+ {
+
+
+
+
+
+
+ }
