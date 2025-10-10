ef724778fd6a9c80c26aafbcecf09d80577f41d1
Bagó Bálint <bago.balint10@gmail.com>
2025-10-11 00:18:56 +0200

    lcd_driver modulok létrehozva

---
+++ robotlampa_final/Inc/lcd_driver.h
@@ -0,0 +1,16 @@
+/*
+ * lcd_driver.h
+ *
+ * Created: 2025. 10. 11. 0:14:10
+ *  Author: bagob
+ */ 
+
+
+#ifndef LCD_DRIVER_H_
+#define LCD_DRIVER_H_
+
+
+
+
+
+#endif /* LCD_DRIVER_H_ */
---
+++ robotlampa_final/Src/lcd_driver.c
@@ -0,0 +1,10 @@
+/*
+ * lcd_driver.c
+ *
+ * Created: 2025. 10. 11. 0:14:24
+ *  Author: bagob
+ */ 
+
+ #include "lcd_driver.h"
+
+
