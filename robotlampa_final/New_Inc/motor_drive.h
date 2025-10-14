1777d4ff9199065df39e732b4452d6937c074826
Bagó Bálint <bago.balint10@gmail.com>
2025-10-14 16:35:26 +0200

    motor_drive modul létrehozva

---
+++ robotlampa_final/New_Inc/motor_drive.h
@@ -0,0 +1,16 @@
+/*
+ * motor_drive.h
+ *
+ * Created: 2025. 10. 14. 16:32:55
+ *  Author: bagob
+ */ 
+
+
+#ifndef MOTOR_DRIVE_H_
+#define MOTOR_DRIVE_H_
+
+
+
+
+
+#endif /* MOTOR_DRIVE_H_ */
---
+++ robotlampa_final/New_Src/motor_drive.c
@@ -0,0 +1,8 @@
+/*
+ * motor_drive.c
+ *
+ * Created: 2025. 10. 14. 16:33:07
+ *  Author: bagob
+ */ 
+
+ #include "motor_drive.h"
